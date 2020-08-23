37d92f38e93846a7ebf330b35700a73036e1b7ba
Vignesh-Jeyaraman <vignesh.jeyaraman@algoworks.com>
2020-08-23 13:13:53 +0530

    combinations

---
+++ combination.py
@@ -0,0 +1,13 @@
+import itertools as it
+
+lst = [1,2,3,4,5,6]
+# combinations take a lst and a pair as input
+itr = it.combinations(lst,2)
+# above line of code will generate an iterator object which will have
+# pair of elements
+# you can access pair either by next(itr) or you have to run a loop over it
+
+for i in itr:
+    print(sum(i)) # sum of i will return sum of the pairs if you want to see pairs just print(i)
+# be careful now if you try to do next(itr) it will throw stopiteration exception and if you try to
+# run a for loop it will not print anything since all the elements in iterator object is consumed
