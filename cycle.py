0c95cc3fd6a4d3b727f84102bbbacbd59b5ef842
Vignesh-Jeyaraman <vignesh.jeyaraman@algoworks.com>
2020-08-23 13:45:22 +0530

    added comments to cycle

--- cycle.py
+++ cycle.py
@@ -1,6 +1,9 @@
 import itertools as it
 
-a = it.cycle([-1,0,1])
+a = it.cycle([-1,0,1]) # pass the list that you want to cycle through
 
+# using next(a) you can values one by one and it is an infinite sequence
+# means if you run an infinite loop and call next(a) it will run forever
+# amazing right.
 for i in range(12):
     print(next(a))
